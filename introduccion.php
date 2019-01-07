a608a66276b88ea8014b44dca30b180525c50a7b
DamasoDaniel <damasodt8a@gmail.com>
2019-01-06 22:42:29 -0600

    Instancia y asignacion de atributos al objeto Curso

--- introduccion.php
+++ introduccion.php
@@ -9,6 +9,7 @@
 	 	}
 	 */
 
+	 #Creación de la clase Curso y sus atributos
 	class Curso{
 		public $nombre;
 		public $duracion;
@@ -17,8 +18,39 @@
 		public $profesor;
 		public $disponible;
 
-		
+
 	}
+
+	#Creación del objeto Curso
+	$php = new Curso();
+
+	#Asignación de los atributos
+	$php->nombre = 'POO en PHP';
+	$php->duracion = '3 sesiones';
+	$php->costo = 10;
+	$php->moneda = 'MXN';
+	$php->profesor = 'Yesi Days';
+	$php->disponible = true;
+
+	var_dump($php);
+
+	$javascript = new Curso();
+	$javascript->nombre = 'javascript desde cero';
+	$javascript->duracion = '6 sesiones';
+	$javascript->costo = 0;
+	$javascript->moneda = 'MXN';
+	$javascript->profesor = 'Álvaro Felipe';
+	$javascript->disponible = true;
+
+	echo "<br>";
+
+	var_dump($javascript);
+
+
+
+
+
+
 
 
 ?>
