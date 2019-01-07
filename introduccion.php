3a8cf95c7ef200848e26cfa73a1184343fbd012b
DamasoDaniel <damasodt8a@gmail.com>
2019-01-06 22:31:01 -0600

    Creación de nuestra primera clase y sus atributos

--- introduccion.php
+++ introduccion.php
@@ -1,4 +1,24 @@
 <?php
 
+	/**
+	 * Sintaxis para crear una clase
+	 Para el nombre de la clase, la primera letra es mayúscula
+	 	class NombreDeMiClase(){
+			Definir propiedades
+			Definir nuestros métodos
+	 	}
+	 */
+
+	class Curso{
+		public $nombre;
+		public $duracion;
+		public $costo;
+		public $moneda;
+		public $profesor;
+		public $disponible;
+
+		
+	}
+
 
 ?>
